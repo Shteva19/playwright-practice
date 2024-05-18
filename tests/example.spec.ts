c509c3e6137afd34c90adba901113efe76b78a56
Yevhen Shtevnin <92028224+Shteva19@users.noreply.github.com>
2024-05-18 04:39:16 +0300

    POM

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,239 +1,198 @@
 import { test, expect } from '@playwright/test';
-
-// test('first test', async ({ page }) => {
-//   await page.goto('/');
-
-//     const pageHeader = page.getByRole('heading', {name:'Do More!'})
-//     await pageHeader.click();
+import { RegistrationForm} from '../page-object/components/loginForm/registrationForm';
+import { LoginData } from '../page-object/components/loginForm/loginData';
 
 
-    
-  
-// });
-
-// test('guest login page', async ({ page }) => {
-//   await page.goto('/');
-
-//     const guestLoginLink = page.getByText('Guest log in' , {exact: true})
-//     await guestLoginLink.click();
-
-
-    
-  
-// });
-
-// test('get by label', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByText('Sign up').click();
-//     const emailField = page.getByLabel('Email');
-//     await emailField.click();
-  
-// });
-
-// test('get by testId', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByText('Sign in').click();
-//   await page.getByTestId('signinEmail').click();
-
-// });
-
-// test('locator', async ({ page }) => {
-//   await page.goto('/');
-//   await page.locator('.header_signin').click();
-
-// });
-
-// test('locator icon', async ({ page }) => {
-//   await page.goto('/');
-//   const instagramIcon  = page.locator(".icon-instagram");
-//   await page.locator('div.row', {has: instagramIcon }).click();
-// });
-
-// test('page.locator().locator();', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByText('Sing up').click();
-//   const form = page.locator('.modal-content');
-//   const buttonInsideForm = form.locator('btn-primary')
-// });
 
 
 ///////////////
 test.describe('Test Name Field',()  =>{
+  let registrationForm : RegistrationForm;
+  let loginData : LoginData;
 
-test('Name is required', async ({ page }) => {
+test.beforeEach(async ({page})=> {
+  registrationForm = new RegistrationForm(page);
+  loginData = new LoginData(page);
   await page.goto('/');
   await page.getByText('Sign up').click();
-  await page.locator('#signupName').focus();
-  await page.locator('#signupName').blur();
+
+  
+})
+
+
+test('Name is required', async ({ page }) => {
+  await registrationForm.clickSignUpButton();
+  await loginData.validName.focus();
+  await loginData.validName.blur();
   await expect(page.getByText('Name required')).toBeVisible();
  
   
 });
 
 test('Name is invalid', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupName').pressSequentially('Євген');
-  await page.locator('#signupLastName').pressSequentially('Shtevnin');
-   await expect(page.getByText('Name is invalid')).toBeVisible();
+  await loginData.inputName('Євген');
+  await loginData.inputLastName('Shtevnin');
+  await expect(page.getByText('Name is invalid')).toBeVisible();
 
 })
 
-test('Wrong length', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupName').fill('Л');
-  await page.locator('#signupLastName').pressSequentially('Shtevnin');
+test('Wrong length name', async ({ page }) => {
+  await loginData.inputName('Л');
+  await loginData.inputLastName('Shtevnin');
    await expect(page.getByText('Name has to be from 2 to 20 characters long')).toBeVisible();
 
 
 })
 test('Border color red Name', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupName').focus();
-  await page.locator('#signupName').blur();
-  await expect(page.locator('#signupName')).toHaveCSS('border-color','rgb(220, 53, 69)');
-
-
+  await loginData.validName.focus();
+  await loginData.validName.blur();
+  await expect(loginData.validName).toHaveCSS('border-color','rgb(220, 53, 69)');
 })
 
 })
 //////////////////////////
  
 test.describe('Test Email Field',()  =>{
+  let registrationForm : RegistrationForm;
+  let loginData : LoginData;
 
+  test.beforeEach(async ({page})=> {
+    registrationForm = new RegistrationForm(page);
+  loginData = new LoginData(page);
+    await page.goto('/');
+    await page.getByText('Sign up').click();
+  
+  })
 test('Email incorrect', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupEmail').fill('jjka@');
-  await page.locator('#signupEmail').blur();
+  await loginData.inputEmail('jjka@');
+  await loginData.validEmail.blur();
   await expect( page.getByText('Email is incorrect')).toBeVisible();
-
-
 })
 test('Email incorrect2', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupEmail').focus();
-  await page.locator('#signupEmail').blur();
+  await loginData.validEmail.focus();
+  await loginData.validEmail.blur();
   await expect(page.getByText('Email required')).toBeVisible();
 })
-
-
 
 })
 //////////////////////////
 
 test.describe('Test lastName Field',()  =>{
-
+  let registrationForm : RegistrationForm;
+  let loginData : LoginData;
+  test.beforeEach(async ({page})=> {
+    registrationForm = new RegistrationForm(page);
+  loginData = new LoginData(page);
+    await page.goto('/');
+    await page.getByText('Sign up').click();
+  
+  })
 test('lastName is required', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupLastName').focus();
-  await page.locator('#signupLastName').blur();
+  await loginData.validLastName.focus();
+  await loginData.validLastName.blur();
   await expect(page.getByText(' Last name required')).toBeVisible();
 
 })
 
 test('lastName is invalid', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupLastName').pressSequentially('Євген');
-  await page.locator('#signupName').pressSequentially('Shtevnin');
+  await loginData.inputLastName('Євген');
+  await loginData.validLastName.blur();
    await expect(page.getByText('Name is invalid')).toBeVisible();
 
 })
 
 test('Wrong length LastName', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupLastName').pressSequentially('S');
-  await page.locator('#signupName').pressSequentially('Yevhen');
+  await loginData.inputLastName('S');
+  await loginData.validLastName.blur();
    await expect(page.getByText('Last name has to be from 2 to 20 characters long')).toBeVisible();
 
 })
 test('Border color red lastName', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupLastName').focus();
-  await page.locator('#signupLastName').blur();
-  await expect(page.locator('#signupLastName')).toHaveCSS('border-color','rgb(220, 53, 69)');
+  await loginData.validLastName.focus();
+  await loginData.validLastName.blur();
+  await expect(loginData.validLastName).toHaveCSS('border-color','rgb(220, 53, 69)');
 
 })
 ///////////////
 
+test.describe('Test Email Field',()  =>{
+  let registrationForm : RegistrationForm;
+  let loginData : LoginData;
+  test.beforeEach(async ({page})=> {
+    registrationForm = new RegistrationForm(page);
+  loginData = new LoginData(page);
+    await page.goto('/');
+    await page.getByText('Sign up').click();
+
+  })
+
 test('Wrong data', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupPassword').pressSequentially('q1');
-  //await page.locator('#signupName').pressSequentially('Shtevnin');
-  await page.locator('#signupPassword').blur();
+  
+  await loginData.inputPassword('q1');
+  await loginData.validPassword.blur();
    await expect(page.getByText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')).toBeVisible();
 
 
 })
 
 test('Empty field error', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupPassword').focus();
-  await page.locator('#signupPassword').blur();
+  await loginData.validPassword.focus();
+  await loginData.validPassword.blur();
   await expect(page.getByText('Password required')).toBeVisible();
 
 })
 
-test('Border color red password', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupPassword').focus();
-  await page.locator('#signupPassword').blur();
-  await expect(page.locator('#signupPassword')).toHaveCSS('border-color','rgb(220, 53, 69)');
+test('Border color red ', async ({ page }) => {
+  await loginData.validPassword.focus();
+  await loginData.validPassword.blur();
+  await expect(loginData.validPassword).toHaveCSS('border-color','rgb(220, 53, 69)');
 })
 
+})
 })
 //////////////////////
 
 test.describe('Test Passwords Field',()  =>{
+  let registrationForm : RegistrationForm;
+  let loginData : LoginData;
+
+
+  test.beforeEach(async ({page})=> {
+    registrationForm = new RegistrationForm(page);
+    loginData = new LoginData(page);
+    await page.goto('/');
+    await page.getByText('Sign up').click();
+  
+  })
 
 test('Passwords not match ', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupPassword').fill('Q1w2e3r4t5y6');
-  await page.locator("#signupRepeatPassword").fill('Q1w2e3r4t5y6!');
-  await page.locator('#signupRepeatPassword').blur();
+  await loginData.inputPassword('Q1w2e3r4t5y6');
+  await loginData.inputRepPassword('Q1w2e3r4t5y6!');
+  await loginData.validReEnterPassword.blur();
   await expect(page.getByText('Passwords do not match')).toBeVisible();
 
 })
 
 test('Re-enter password ', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupPassword').fill('Q1w2e3r4t5y6');
-  await page.locator("#signupRepeatPassword").fill('');
-  await page.locator('#signupRepeatPassword').blur();
+  await loginData.inputPassword('Q1w2e3r4t5y6');
+  await loginData.validReEnterPassword.fill('');
+  await loginData.validReEnterPassword.blur();
   await expect(page.getByText('Re-enter password required')).toBeVisible(); 
 
 
 
 })
 test('Border color red Re-enter password', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupRepeatPassword').focus();
-  await page.locator('#signupRepeatPassword').blur();
-  await expect(page.locator('#signupRepeatPassword')).toHaveCSS('border-color','rgb(220, 53, 69)');
+  
+  await loginData.validReEnterPassword.focus();
+  await loginData.validReEnterPassword.blur();
+  await expect(loginData.validReEnterPassword).toHaveCSS('border-color','rgb(220, 53, 69)');
 })
 
 test('Registration ', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('Sign up').click();
-  await page.locator('#signupName').fill('Yevhen');
-  await page.locator('#signupLastName').fill('Shtev');
-  await page.locator('#signupEmail').fill('maybeimala+aqa123@gmail.com');
-  await page.locator('#signupPassword').fill('Q1w2e3r4t5y6');
-  await page.locator('#signupRepeatPassword').fill('Q1w2e3r4t5y6');
-  await page.getByText('Register').click();
+  await loginData.loginRegistrationForm('Yevhen','Shtev','maybeimala+aqa123@gmail.com','Q1w2e3r4t5y6','Q1w2e3r4t5y6');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByText('User already exists')).toBeVisible(); 
   
 })
 
